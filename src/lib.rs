3ad9c3dc2f5ab2217c7bfbd7c637a92b82bab46e
Tamme Schichler <tamme@schichler.dev>
2020-08-21 11:44:11 +0200

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -32,82 +32,84 @@
 
 #![warn(clippy::pedantic)]
 
-use {gnaw::Unshift as _, smartstring::alias::String, woc::Woc};
+use gnaw::Unshift as _;
+use smartstring::alias::String;
+use woc::Woc;
 
 pub enum TransformedPart {
-    Unchanged,
-    Changed(String),
+	Unchanged,
+	Changed(String),
 }
 
 pub fn transform(
-    str: &str,
-    transform_next: impl FnMut(/* rest: */ &mut &str) -> TransformedPart,
+	str: &str,
+	transform_next: impl FnMut(/* rest: */ &mut &str) -> TransformedPart,
 ) -> Woc<String, str> {
-    str.transform(transform_next)
+	str.transform(transform_next)
 }
 
 pub trait Transform {
-    fn transform(
-        &self,
-        transform_next: impl FnMut(&mut &str) -> TransformedPart,
-    ) -> Woc<String, str>;
+	fn transform(
+		&self,
+		transform_next: impl FnMut(&mut &str) -> TransformedPart,
+	) -> Woc<String, str>;
 }
 
 impl Transform for str {
-    fn transform(
-        &self,
-        mut transform_next: impl FnMut(&mut &str) -> TransformedPart,
-    ) -> Woc<String, str> {
-        let mut rest = self;
-        let mut copied = loop {
-            if rest.is_empty() {
-                return Woc::Borrowed(self);
-            }
-            let unchanged_rest = rest;
-            if let TransformedPart::Changed(transformed) = transform_next(&mut rest) {
-                let mut copied = String::from(&self[..self.len() - unchanged_rest.len()]);
-                copied.push_str(&transformed);
-                break copied;
-            }
-        };
+	fn transform(
+		&self,
+		mut transform_next: impl FnMut(&mut &str) -> TransformedPart,
+	) -> Woc<String, str> {
+		let mut rest = self;
+		let mut copied = loop {
+			if rest.is_empty() {
+				return Woc::Borrowed(self);
+			}
+			let unchanged_rest = rest;
+			if let TransformedPart::Changed(transformed) = transform_next(&mut rest) {
+				let mut copied = String::from(&self[..self.len() - unchanged_rest.len()]);
+				copied.push_str(&transformed);
+				break copied;
+			}
+		};
 
-        while !rest.is_empty() {
-            let unchanged_rest = rest;
-            match transform_next(&mut rest) {
-                TransformedPart::Unchanged => {
-                    copied.push_str(&unchanged_rest[..unchanged_rest.len() - rest.len()]);
-                }
-                TransformedPart::Changed(changed) => copied.push_str(&changed),
-            }
-        }
+		while !rest.is_empty() {
+			let unchanged_rest = rest;
+			match transform_next(&mut rest) {
+				TransformedPart::Unchanged => {
+					copied.push_str(&unchanged_rest[..unchanged_rest.len() - rest.len()]);
+				}
+				TransformedPart::Changed(changed) => copied.push_str(&changed),
+			}
+		}
 
-        Woc::Owned(copied)
-    }
+		Woc::Owned(copied)
+	}
 }
 
 #[must_use = "pure function"]
 pub fn escape_double_quotes(string: &str) -> Woc<String, str> {
-    string.transform(|rest| match rest.unshift().unwrap() {
-        c @ '\\' | c @ '"' => {
-            let mut changed = String::from(r"\");
-            changed.push(c);
-            TransformedPart::Changed(changed)
-        }
-        _ => TransformedPart::Unchanged,
-    })
+	string.transform(|rest| match rest.unshift().unwrap() {
+		c @ '\\' | c @ '"' => {
+			let mut changed = String::from(r"\");
+			changed.push(c);
+			TransformedPart::Changed(changed)
+		}
+		_ => TransformedPart::Unchanged,
+	})
 }
 
 #[must_use = "pure function"]
 pub fn unescape_backslashed_verbatim(string: &str) -> Woc<String, str> {
-    let mut escaped = false;
-    string.transform(|rest| match rest.unshift().unwrap() {
-        '\\' if !escaped => {
-            escaped = true;
-            TransformedPart::Changed(String::new())
-        }
-        _ => {
-            escaped = false;
-            TransformedPart::Unchanged
-        }
-    })
+	let mut escaped = false;
+	string.transform(|rest| match rest.unshift().unwrap() {
+		'\\' if !escaped => {
+			escaped = true;
+			TransformedPart::Changed(String::new())
+		}
+		_ => {
+			escaped = false;
+			TransformedPart::Unchanged
+		}
+	})
 }
